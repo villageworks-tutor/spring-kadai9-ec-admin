cfb67364700797dd457e223a1b5997c7dbef73c7
villageworks <villageworks@gmail.com>
2024-01-28 23:37:02 +0900

    ver.1.00: プロダクト初版

---
+++ src/main/java/com/example/demo/model/Account.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Account {
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
