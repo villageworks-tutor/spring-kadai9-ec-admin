45a55c2078fa1addd8a3ae9d3bf3e417e74b9b81
villageworks <villageworks@gmail.com>
2024-01-29 17:25:59 +0900

    ver.1.32: ログイン処理

--- src/main/java/com/example/demo/controller/admin/AccountAdminController.java
+++ src/main/java/com/example/demo/controller/admin/AccountAdminController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -13,5 +17,20 @@ public class AccountAdminController {
 		// 画面遷移
 		return "admin/login";
 	}
+	
+	@PostMapping("/admin/login")
+	public String login(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			Model model) {
+		// ログイン認証
+		if (!("admin".equals(name) && "himitu".equals(password))) {
+			model.addAttribute("error", "ユーザ名とパスワードが一致しませんできた。");
+			return "/admin/login";
+		}
+		// 商品一覧表示にリダイレクト
+		return "redirect:/admin/items";
+	}
+	
 	
 }
